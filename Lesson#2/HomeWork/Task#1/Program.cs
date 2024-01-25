0f88ff938e61a0870a6b6d07f8ab7efbd1cf9360
Yeren Daniyar <y.daniyar.official@gmail.com>
2024-01-26 00:51:16 +0600

    HomeWork Lesson#2

---
+++ Lesson#2/HomeWork/Task#1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter Number");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
---
+++ Lesson#2/HomeWork/Task#3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число из отрезка [10, 99]: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int firstnumber = number / 10;
+int secondnumber = number % 10;
+int maxDigit = firstnumber > secondnumber ? firstnumber : secondnumber;
+
+Console.WriteLine(maxDigit);
